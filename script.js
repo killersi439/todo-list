dd7a7e383028e4b9e5cee029084a151cee4b1f98
killersi439 <kevinmwaniki439@gmail.com>
2025-04-06 21:20:50 +0300

    writing js

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-console .log("Hello, World!");
+// console .log("Hello, World!");
 // we need html elements
 // -input
 // -button
+ const dolist1 = document.getElementById("dolist")
+ const submitbtn = document.getElementById("submit")
+ const tasklist1 = document.getElementById("tasklist")
+
+ submitbtn.addEventListener("click",function(){
+    const WhatUserHasEntered = dolist1.ariaValueMax
+    // validation
+    if(WhatUserHasEntered ===""){
+        return alert("Please enter somthing")
+    }
+    tasklist1.push(WhatUserHasEntered)
+    RenderItems()
+    dolist1.value = ""
+ })
+
+ function RenderItems(){
+    tasklist1.innerHTML =""
+    for(let i = 0 ; i < tasklist1.clientHeight; i++ ){
+        const newLiEl =document.createElement("li")
+        newLiEl.innerHTML = tasklist1[i]
+        tasklist1.appendChild(newLiEl)
+    
+    }
+ }
